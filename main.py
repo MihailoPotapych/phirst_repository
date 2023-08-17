d080e1432145b97b11628cc52ff40545fc5345b1
Mihailo <mihailkajgorodov955@gmail.com>
2023-08-17 14:10:44 +0300

    Это new комментарий

--- main.py
+++
@@ -1,7 +0,0 @@
-def is_palindrome(word):
-    word = word.lower()
-    for i in range(len(word)//2):
-        print(word[i], word[-(i + 1)])
-        if not word[i] == word[-i-1]:
-            return False
-    return True
